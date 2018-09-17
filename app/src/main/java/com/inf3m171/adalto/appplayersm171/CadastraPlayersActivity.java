68aa806d2ff8585e4d046496c7cb41a33bfbbee8
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-09-17 09:33:06 -0300

    Desenvolvimento das telas

---
+++ app/src/main/java/com/inf3m171/adalto/appplayersm171/CadastraPlayersActivity.java
@@ -0,0 +1,13 @@
+package com.inf3m171.adalto.appplayersm171;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastraPlayersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_players);
+    }
+}
